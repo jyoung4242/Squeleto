15df04ccc90649fa3d156684a14a78d91832b872
Justin Young <justin_dean_young@yahoo.com>
2023-09-17 10:08:49 -0400

    updated _component for opacity

--- srcECS/Components/_components.ts
+++ srcECS/Components/_components.ts
@@ -9,10 +9,11 @@
 import { Position } from "./positionComponent";
 import { ZindexComp } from "./zindexComponent";
 import { SizeComp } from "./sizeComponent";
+import { OpacityComp } from "./opacityComponent";
 
 // The template component is demonstrated by default, you'll probably
 // want to replace it
 
 export function LoadComponents() {
-  [new Position(), new ZindexComp(), new SizeComp()];
+  [new Position(), new ZindexComp(), new SizeComp(), new OpacityComp()];
 }
